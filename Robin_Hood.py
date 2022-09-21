6fe679d7905be55a82e1cce807139f6b834c1fb4
Christian <112812176+RealXun@users.noreply.github.com>
2022-09-21 12:05:55 +0200

    Update Robin_Hood.py

--- Robin_Hood.py
+++ Robin_Hood.py
@@ -3,8 +3,8 @@ puntos = [(4,5), (-0,2), (4,7), (1,-3), (3,-2), (4,5), (3,2), (5,7), (-5,7), (2,
 
 #1. Robin Hood is famous for hitting an arrow with another arrow. Has he got it?
 points = [(4,5), (-0,2), (4,7), (1,-3), (3,-2), (4,5), (3,2), (5,7), (-5,7), (2,2), (-4,5), (0,-2), (-4,7), (-1,3), (-3,2), (-4,-5), (-3,2), (5,7), (5,7), (2,2), (9, 9), (-8, -9)]
-res = set([point for point in points if points.count(point) > 1])
-print("Robin Hood hitted this arrow with another arrow : " + str(res))
+result = set([point for point in points if points.count(point) > 1])
+print("Robin Hood hitted this arrow with another arrow : " + str(result))
 # 2. Calculate how many arrows have fallen in each quadrant.
 q1 = []
 q2 = []
